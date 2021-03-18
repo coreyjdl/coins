e97268b2970c4a6d2b6a6d777a7a64ab2a4f17f8
coreyjdl <coreyjdl@gmail.com>
2021-03-17 22:27:57 -0500

    formating

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const http = require('http');
 const app = require('./app');
 
 const server = http.createServer(app);
-
 const port = 3000
-server.listen(port, () => {console.log('started server on port ' + port)});
+
+server.listen(port, () => {
+  console.log('started server on port ' + port)
+});
